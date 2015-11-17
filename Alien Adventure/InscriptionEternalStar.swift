c5cb1e158ef04f49e3ebfe5110920d39da293412
swaption2009 <swaption2009@gmail.com>
2015-11-16 22:56:41 -0800

    refactor Inscription codes

--- Alien Adventure/InscriptionEternalStar.swift
+++ Alien Adventure/InscriptionEternalStar.swift
@@ -9,20 +9,13 @@
 extension Hero {
     
     func inscriptionEternalStar(inventory: [UDItem]) -> UDItem? {
-        var inscriptionItems: UDItem?
         
         for i in inventory {
             if i.inscription == "THE GLOW OF THE ETERNAL STAR" {
-                // print(i.inscription)
                 if let inscriptionItem = i.inscription {
                     let x = inscriptionItem
                     return i
-                    // print(x)
-                } else {
-                    print("no inscription")
                 }
-            } else {
-                print("nothing's here")
             }
         }
         
