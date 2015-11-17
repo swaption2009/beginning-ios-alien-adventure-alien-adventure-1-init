ab446c113c692ded3c57ed93fc8957d8e60aae80
swaption2009 <swaption2009@gmail.com>
2015-11-16 22:13:23 -0800

    return optional data type
    find External Star from inscription and return value

--- Alien Adventure/InscriptionEternalStar.swift
+++ Alien Adventure/InscriptionEternalStar.swift
@@ -9,6 +9,23 @@
 extension Hero {
     
     func inscriptionEternalStar(inventory: [UDItem]) -> UDItem? {
+        var inscriptionItems: UDItem?
+        
+        for i in inventory {
+            if i.inscription == "THE GLOW OF THE ETERNAL STAR" {
+                // print(i.inscription)
+                if let inscriptionItem = i.inscription {
+                    let x = inscriptionItem
+                    return i
+                    // print(x)
+                } else {
+                    print("no inscription")
+                }
+            } else {
+                print("nothing's here")
+            }
+        }
+        
         return nil
     }
 }
