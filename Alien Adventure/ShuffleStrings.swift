549f3c8cfa7d77a124f9b46aa2d7cef6b953710b
swaption2009 <swaption2009@gmail.com>
2015-11-19 12:59:35 -0800

    Complete P3 final test

--- Alien Adventure/ShuffleStrings.swift
+++ Alien Adventure/ShuffleStrings.swift
@@ -9,6 +9,63 @@
 extension Hero {
     
     func shuffleStrings(s1 s1: String, s2: String, shuffle: String) -> Bool {
-        return true
+
+        // set bool conditions
+        var lengthRequirement = false
+        var isPass = false
+        var s1Switch = true
+        var s2Switch = true
+        
+        var indexArrayS1 = Array<String.CharacterView.Index>()
+        var indexArrayS2 = Array<String.CharacterView.Index>()
+        
+        // first check: sum of character count
+        if shuffle.characters.count == s1.characters.count + s2.characters.count {
+            lengthRequirement = true
+        }
+        
+        // index s1 characters in shuffle
+        for character in s1.characters {
+            for index in shuffle.characters.indices {
+                if character == shuffle[index] {
+                    indexArrayS1.append(index)
+                }
+            }
+        }
+        
+        // index s2 characters in shuffle
+        for character in s2.characters {
+            for index in shuffle.characters.indices {
+                if character == shuffle[index] {
+                    indexArrayS2.append(index)
+                }
+            }
+        }
+        
+//        print(indexArrayS1)
+//        print(indexArrayS2)
+        
+        var test1 = shuffle.characters.startIndex
+        var test2 = shuffle.characters.startIndex
+        
+        for value in indexArrayS1 {
+            if test1 > value {
+                s1Switch = false
+            }
+            test1 = value
+        }
+        
+        for value in indexArrayS2 {
+            if test2 > value {
+                s2Switch = false
+            }
+            test2 = value
+        }
+        
+        // check if isPass follows all requirements
+        if s1Switch && s2Switch && lengthRequirement {
+            isPass = true
+        }
+        return isPass
     }
 }
