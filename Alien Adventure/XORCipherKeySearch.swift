c3a731ae12208b581712fd2f17b0fb6bf5bc555a
swaption2009 <swaption2009@gmail.com>
2015-11-27 21:33:05 -0800

    iOS P4 q3: XOR Cipher function

--- Alien Adventure/XORCipherKeySearch.swift
+++ Alien Adventure/XORCipherKeySearch.swift
@@ -11,6 +11,34 @@ import Foundation
 extension Hero {
     
     func xorCipherKeySearch(encryptedString: [UInt8]) -> UInt8 {
-        return 0
+        var key: UInt8 = 0
+        
+        for x in UInt8.min..<UInt8.max {
+            
+//            print(x)
+            
+            var decrypted = [UInt8]()
+            
+            for character in encryptedString {
+                
+//              print(character)
+                
+                // ADD CODE: perform decryption
+                var result1 = character ^ x
+//               print(result1)
+                decrypted.append(result1)
+            }
+            
+//            print(decrypted)
+            
+            if let decryptedString = String(bytes: decrypted,
+                encoding: NSUTF8StringEncoding) where decryptedString == "udacity" {
+                    // ADD CODE: found match, now what?
+//                  print("found")
+                    key = x
+            }
+        }
+        
+        return key
     }
 }
